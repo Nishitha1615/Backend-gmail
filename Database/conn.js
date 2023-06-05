3b8bcf096ac1515db829e0af2a71753117c2fc3c
Nishitha1615 <nishitham0@gmail.com>
2023-06-05 19:03:49 +0530

    small-change

--- Database/conn.js
+++ Database/conn.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose"
 
 const Connection=()=>{
     try{
-        mongoose.connect('mongodb+srv://abimeeraperumal10:Jj2sJvpQvSegG4m8@nishitha.8jv94cv.mongodb.net/GmailClone',{useNewUrlParser:true});
+        mongoose.connect('mongodb+srv://abimeeraperumal10:Jj2sJvpQvSegG4m8@nishitha.8jv94cv.mongodb.net/GmailClone',{useUnifiedTopology: true,useNewUrlParser:true});
         console.log("DB CONNECTED");
     }
     catch(e){
